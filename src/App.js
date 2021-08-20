d5a8a1aa7c04a81dbba865bff22efd92f912d38b
amit1906 <1amitg@walla.com>
2021-08-20 10:45:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Fotter from "./Components/Fotter";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Notes from "./Components/Notes";
+import Home from "./Components/Home";
+import React from "react";
 
 function App() {
   return (
-    <Router>
+    <React.Fragment>
       <Header />
-      <Notes />
+      <Route path="/" component={Home} />
+      <Route path="/sentences" component={Notes} />
       <Fotter />
-    </Router>
+    </React.Fragment>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <h1>
+                                                                                                         ...בהכנה
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
