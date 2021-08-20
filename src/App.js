e7d477bfb03408428e6b2e6acfe5c1707a7262df
amit1906 <1amitg@walla.com>
2021-08-20 18:37:05 +0300

    update to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import "./App.css";
-import Header from "./Components/Header";
-import Fotter from "./Components/Fotter";
-import { Route, Redirect } from "react-router-dom";
-import Home from "./Components/Home";
-import Notes from "./Components/Notes";
-import Revenges from "./Components/Revenges";
-import Characters from "./Components/Characters";
-import React from "react";
+import './App.css';
+import Header from './Components/Header';
+import Fotter from './Components/Fotter';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import Home from './Components/Home';
+import Notes from './Components/Notes';
+import Revenges from './Components/Revenges';
+import Characters from './Components/Characters';
+import React from 'react';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      <Route path="/home" component={Home} />
-      <Route path="/revenges" component={Revenges} />
-      <Route path="/characters" component={Characters} />
-      <Route path="/sentences" component={Notes} />
 
-      <Route path="/" dir>
-        <Redirect to="/home" />
-      </Route>
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/revenges" component={Revenges} />
+        <Route path="/characters" component={Characters} />
+        <Route path="/sentences" component={Notes} />
+
+        <Route path="" dir>
+          <Redirect to="/home" />
+        </Route>
+      </Switch>
+
       <Fotter />
     </React.Fragment>
   );
